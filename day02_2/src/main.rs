6497d84a2a9867852f97bfb6cf2b32b681f51719
Nicholas L <nicholas-l@users.noreply.github.com>
2019-12-28 14:21:17 +0000

    Few changes in day02_2

--- day02_2/src/main.rs
+++ day02_2/src/main.rs
@@ -1,23 +1,18 @@
 use std::fs;
 
-fn process(mut codes: Vec<usize>) -> Vec<usize> {
+fn process(codes: &mut Vec<usize>) -> usize {
     let mut index = 0;
     while codes[index] != 99 {
-        let input1_index = codes[index + 1];
-        let input2_index = codes[index + 2];
         let output_index = codes[index + 3];
         match codes[index] {
-            1 => {
-                codes[output_index] = codes[input1_index] + codes[input2_index];
-            }
-            2 => {
-                codes[output_index] = codes[input1_index] * codes[input2_index];
-            }
-            _ => todo!(),
+            1 => codes[output_index] = codes[codes[index + 1]] + codes[codes[index + 2]],
+            2 => codes[output_index] = codes[codes[index + 1]] * codes[codes[index + 2]],
+            99 => break,
+            _ => panic!(),
         }
         index += 4;
     }
-    return codes;
+    return codes[0];
 }
 
 fn main() {
@@ -26,15 +21,15 @@ fn main() {
         .split(",")
         .map(|v| v.parse::<usize>().unwrap())
         .collect();
-    println!("{:?}", codes);
+    // println!("{:?}", codes);
     for noun in 1..100 {
         for verb in 1..100 {
             let mut code = codes.clone();
             code[1] = noun;
             code[2] = verb;
-            let code2 = process(code);
+            let output = process(&mut code);
 
-            if code2[0] == 19690720 {
+            if output == 19690720 {
                 println!("{} {}", noun, verb);
                 return;
             }
