38042f39b76eea96499aa6b2d522eb89f444f945
Nicholas L <nicholas-l@users.noreply.github.com>
2021-06-04 15:10:16 +0100

    Convert to use new IntCode struct

--- src/lib.rs
+++ src/lib.rs
@@ -12,16 +12,8 @@ pub mod day09;
 pub mod day10;
 pub mod day11;
 
-fn write(program: &mut Vec<isize>, index: usize, data: isize) {
-    if index >= program.len() {
-        program.resize(index + 1, 0)
-    }
-    program[index] = data;
-}
 
-fn read(program: &[isize], index: usize) -> isize {
-    *program.get(index).unwrap_or(&0)
-}
+
 
 struct IntCode<'a> {
     program: &'a mut Vec<isize>,
@@ -54,6 +46,11 @@ impl IntCode<'_> {
     fn len(&self) -> usize {
         self.program.len()
     }
+
+    fn read(&self, index: usize) -> isize {
+        *self.program.get(index).unwrap_or(&0)
+    }
+
     fn write(&mut self, index: usize, data: isize) {
         if index >= self.program.len() {
             self.program.resize(index + 1, 0)
@@ -74,7 +71,7 @@ impl IntCode<'_> {
         self.output.drain(..).collect()
     }
     fn get_parameter(&self, op: usize, pos: usize) -> isize {
-        read(&self.program, self.get_index(op, pos))
+        self.read(self.get_index(op, pos))
     }
     fn run(&mut self, stop_if_output: bool) -> (usize, bool) {
         let mut found_99 = false;
@@ -181,118 +178,11 @@ pub fn process(
     input: &mut Vec<isize>,
     stop_if_output: bool,
 ) -> (usize, Vec<isize>, bool) {
-    let mut index = start_index;
-    let mut output = vec![];
-    let mut found_99 = false;
-    let mut relative_base: isize = 0;
+    let mut computer = IntCode::new(codes, start_index, input.to_vec());
 
-    let get_index = |program: &[isize], op, pos: usize, relative_base| {
-        // println!("{:?} {} {} {}", program, op, pos, (program[op] / (10*10_isize.pow(pos as u32))) % 10);
-        let mode = (program[op] / 10_isize.pow((pos + 1).try_into().unwrap())) % 10;
-        match mode {
-            0 => program[op + pos] as usize,
-            1 => op + pos,
-            2 => (relative_base + program[op + pos]) as usize,
-            _ => panic!("Mode {} not supported", mode),
-        }
-    };
-    let get_parameter = |program: &[isize], op, pos: usize, relative_base| {
-        // println!("{:?} {} {} {}", program, op, pos, (program[op] / (10*10_isize.pow(pos as u32))) % 10);
-        read(program, get_index(program, op, pos, relative_base))
-    };
-    while index < codes.len() {
-        match codes[index] % 100 {
-            1 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                let input2 = get_parameter(codes, index, 2, relative_base);
-                let output_index = get_index(codes, index, 3, relative_base);
-                // assert!(input1 >= 0);
-                // assert!(input2 >= 0);
-                write(codes, output_index, input1 + input2);
-                index += 4;
-            }
-            2 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                let input2 = get_parameter(codes, index, 2, relative_base);
-                let output_index = get_index(codes, index, 3, relative_base);
-                write(codes, output_index, input1 * input2);
-                index += 4;
-            }
-            3 => {
-                let output_index = get_index(codes, index, 1, relative_base);
-                codes[output_index] = input.remove(0);
-                index += 2
-            }
-            4 => {
-                let output_parameter = get_parameter(codes, index, 1, relative_base);
-                output.push(output_parameter);
-                index += 2;
-                if stop_if_output {
-                    break;
-                }
-            }
-            //Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-            5 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                let input2 = get_parameter(codes, index, 2, relative_base);
-                if input1 > 0 {
-                    index = input2 as usize
-                } else {
-                    index += 3
-                }
-            }
-            // Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-            6 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                let input2 = get_parameter(codes, index, 2, relative_base);
-                if input1 == 0 {
-                    index = input2 as usize
-                } else {
-                    index += 3
-                }
-            }
-            // Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-            7 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                let input2 = get_parameter(codes, index, 2, relative_base);
-                let output_index = get_index(codes, index, 3, relative_base);
-                if input1 < input2 {
-                    codes[output_index] = 1;
-                } else {
-                    codes[output_index] = 0;
-                }
-                index += 4;
-            }
-            // Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-            8 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                let input2 = get_parameter(codes, index, 2, relative_base);
-                let output_index = get_index(codes, index, 3, relative_base);
+    let (index, found_99) = computer.run(stop_if_output);
 
-                if input1 == input2 {
-                    write(codes, output_index, 1);
-                } else {
-                    write(codes, output_index, 0);
-                }
-                index += 4
-            }
-            9 => {
-                let input1 = get_parameter(codes, index, 1, relative_base);
-                relative_base += input1;
-                index += 2
-            }
-            // Halt program
-            99 => {
-                found_99 = true;
-                break; // return None,
-            }
-            x => {
-                println!("{:?} [{}]: {}", codes, index, x);
-                panic!()
-            }
-        }
-    }
-    (index, output, found_99)
+    (index, computer.take_output(), found_99)
 }
 
 #[cfg(test)]
