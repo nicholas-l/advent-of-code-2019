ffb6f2aa9450bce296c17e9604a150a3b3cabb5a
Nicholas L <nicholas-l@users.noreply.github.com>
2021-06-20 20:20:05 +0100

    Tidy up Day 20 Part 1

--- src/day20.rs
+++ src/day20.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io::BufRead};
+use std::{collections::HashMap, io::BufRead, cmp::Reverse};
 
 #[derive(Debug, Hash, PartialEq, Eq)]
 enum Tile {
@@ -27,7 +27,7 @@ fn get_teleporter(map: &[Vec<char>], x: usize, y: usize) -> Option<(char, char)>
     None
 }
 
-pub fn star_one(input: impl BufRead) -> usize {
+fn get_map(input: impl BufRead) -> Vec<Vec<Tile>> {
     let raw_map: Vec<Vec<char>> = input
         .lines()
         .map(|v| {
@@ -36,7 +36,7 @@ pub fn star_one(input: impl BufRead) -> usize {
         })
         .collect();
     let raw_height = raw_map.len();
-    let map: Vec<Vec<Tile>> = raw_map
+    raw_map
         .iter()
         .enumerate()
         .skip(2)
@@ -63,15 +63,19 @@ pub fn star_one(input: impl BufRead) -> usize {
                 })
                 .collect()
         })
-        .collect();
+        .collect()
+}
+
+pub fn star_one(input: impl BufRead) -> usize {
+    let map = get_map(input);
     let mut teleporter_positions = HashMap::new();
     for (y, row) in map.iter().enumerate() {
         for (x, c) in row.iter().enumerate() {
-            match c {
-                Tile::Wall => print!("#"),
-                Tile::Empty => print!("."),
-                Tile::Teleport(a, _) => print!("{}", a),
-            }
+            // match c {
+            //     Tile::Wall => print!("#"),
+            //     Tile::Empty => print!("."),
+            //     Tile::Teleport(a, _) => print!("{}", a),
+            // }
             if let Tile::Teleport(_a, _b) = c {
                 teleporter_positions
                     .entry(c)
@@ -79,9 +83,9 @@ pub fn star_one(input: impl BufRead) -> usize {
                     .push((x, y));
             }
         }
-        println!()
+        // println!()
     }
-    println!("{:?}", teleporter_positions);
+    // println!("{:?}", teleporter_positions);
     let start = teleporter_positions.get(&Tile::Teleport('A', 'A')).unwrap()[0];
 
     let mut stack = vec![
@@ -138,50 +142,15 @@ pub fn star_one(input: impl BufRead) -> usize {
 }
 
 pub fn star_two(input: impl BufRead) -> usize {
-    let raw_map: Vec<Vec<char>> = input
-        .lines()
-        .map(|v| {
-            // println!("{}", &v);
-            v.unwrap().chars().collect::<Vec<char>>()
-        })
-        .collect();
-    let raw_height = raw_map.len();
-    let map: Vec<Vec<Tile>> = raw_map
-        .iter()
-        .enumerate()
-        .skip(2)
-        .take(raw_height - 4)
-        .map(|(y, row)| {
-            let raw_width = row.len();
-            row.iter()
-                .enumerate()
-                .skip(2)
-                .take(raw_width - 4)
-                .map(|(x, c)| {
-                    match *c {
-                        '.' => {
-                            // Check if teleporter
-                            get_teleporter(&raw_map, x, y)
-                                .map(|(a, b)| Tile::Teleport(a, b))
-                                .unwrap_or(Tile::Empty)
-                        }
-                        '#' => Tile::Wall,
-                        'A'..='Z' | 'a'..='z' => Tile::Wall,
-                        ' ' => Tile::Wall,
-                        c => panic!("{:?} at ({}, {})", c, x, y),
-                    }
-                })
-                .collect()
-        })
-        .collect();
+    let map = get_map(input);
     let mut teleporter_positions = HashMap::new();
     for (y, row) in map.iter().enumerate() {
         for (x, c) in row.iter().enumerate() {
-            match c {
-                Tile::Wall => print!("#"),
-                Tile::Empty => print!("."),
-                Tile::Teleport(a, _) => print!("{}", a),
-            }
+            // match c {
+            //     Tile::Wall => print!("#"),
+            //     Tile::Empty => print!("."),
+            //     Tile::Teleport(a, _) => print!("{}", a),
+            // }
             if let Tile::Teleport(_a, _b) = c {
                 teleporter_positions
                     .entry(c)
@@ -189,9 +158,81 @@ pub fn star_two(input: impl BufRead) -> usize {
                     .push((x, y));
             }
         }
-        println!()
+        // println!()
     }
-    todo!()
+
+    let start = teleporter_positions.get(&Tile::Teleport('A', 'A')).unwrap()[0];
+
+    let mut stack = vec![
+        ((start.0.wrapping_sub(1), start.1), 0, 1),
+        ((start.0 + 1, start.1), 0, 1),
+        ((start.0, start.1.wrapping_sub(1)), 0, 1),
+        ((start.0, start.1 + 1), 0, 1),
+    ];
+
+    let mut costs = vec![vec![HashMap::<usize, usize>::default(); map[0].len()]; map.len()];
+
+    costs[start.1][start.0].insert(0, 0);
+
+    while let Some(((x, y), level, steps)) = stack.pop() {
+        // println!("Looking at ({}, {})", x, y);
+        match (level, map.get(y).and_then(|row| row.get(x))) {
+            (_, None) => {}
+            (_level, Some(Tile::Wall)) => {}
+
+            (level, Some(Tile::Empty)) => {
+                if *costs[y][x].get(&level).unwrap_or(&usize::MAX) > steps {
+                    costs[y][x].insert(level, steps);
+                    // add surrounding tiles
+                    stack.push(((x - 1, y), level, steps + 1));
+                    stack.push(((x + 1, y), level, steps + 1));
+
+                    stack.push(((x, y - 1), level, steps + 1));
+                    stack.push(((x, y + 1), level, steps + 1));
+                }
+            }
+            (0, Some(Tile::Teleport('Z', 'Z'))) => {
+                if *costs[y][x].get(&level).unwrap_or(&usize::MAX) > steps {
+                    costs[y][x].insert(level, steps);
+                    // add surrounding tiles
+                    stack.push(((x.wrapping_sub(1), y), level, steps + 1));
+                    stack.push(((x + 1, y), level, steps + 1));
+
+                    stack.push(((x, y.wrapping_sub(1)), level, steps + 1));
+                    stack.push(((x, y + 1), level, steps + 1));
+                }
+                break;
+            }
+            (_, Some(Tile::Teleport('Z', 'Z'))) => {}
+            (_, Some(Tile::Teleport('A', 'A'))) => {}
+            (level, Some(Tile::Teleport(a, b))) => {
+                let is_outer = x == 0 || y == 0 || x == (map[0].len() - 1) || y == (map.len() - 1);
+                if *costs[y][x].get(&level).unwrap_or(&usize::MAX) > steps && !(level == 0 && is_outer) {
+                    costs[y][x].insert(level, steps);
+                    // add teleported position to stack
+                    let new_positions = teleporter_positions.get(&map[y][x]).unwrap();
+                    let new_level = if is_outer {
+                        level - 1
+                    } else {
+                        level + 1
+                    };
+                    assert_eq!(new_positions.len(), 2, "Panic'd at {} {}", a, b);
+                    let (new_x, new_y) = new_positions.iter().find(|&p| p != &(x, y)).unwrap();
+                    costs[*new_y][*new_x].insert(new_level, steps + 1);
+                    let x = *new_x;
+                    let y = *new_y;
+                    stack.push(((x.wrapping_sub(1), y), new_level, steps + 2));
+                    stack.push(((x + 1, y), new_level, steps + 2));
+
+                    stack.push(((x, y.wrapping_sub(1)), new_level, steps + 2));
+                    stack.push(((x, y + 1), new_level, steps + 2));
+                }
+            }
+        }
+        stack.sort_by_cached_key(|x| Reverse(x.2));
+    }
+    let end = teleporter_positions.get(&Tile::Teleport('Z', 'Z')).unwrap()[0];
+    *costs[end.1][end.0].get(&0).unwrap()
 }
 
 #[cfg(test)]
@@ -257,6 +298,44 @@ YN......#               VT..#....QG
            B   J   C               
            U   P   P               ";
 
+    const INPUT3: &str = "             Z L X W       C                 
+             Z P Q B       K                 
+  ###########.#.#.#.#######.###############  
+  #...#.......#.#.......#.#.......#.#.#...#  
+  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  
+  #.#...#.#.#...#.#.#...#...#...#.#.......#  
+  #.###.#######.###.###.#.###.###.#.#######  
+  #...#.......#.#...#...#.............#...#  
+  #.#########.#######.#.#######.#######.###  
+  #...#.#    F       R I       Z    #.#.#.#  
+  #.###.#    D       E C       H    #.#.#.#  
+  #.#...#                           #...#.#  
+  #.###.#                           #.###.#  
+  #.#....OA                       WB..#.#..ZH
+  #.###.#                           #.#.#.#  
+CJ......#                           #.....#  
+  #######                           #######  
+  #.#....CK                         #......IC
+  #.###.#                           #.###.#  
+  #.....#                           #...#.#  
+  ###.###                           #.#.#.#  
+XF....#.#                         RF..#.#.#  
+  #####.#                           #######  
+  #......CJ                       NM..#...#  
+  ###.#.#                           #.###.#  
+RE....#.#                           #......RF
+  ###.###        X   X       L      #.#.#.#  
+  #.....#        F   Q       P      #.#.#.#  
+  ###.###########.###.#######.#########.###  
+  #.....#...#.....#.......#...#.....#.#...#  
+  #####.#.###.#######.#######.###.###.#.#.#  
+  #.......#.......#.#.#.#.#...#...#...#.#.#  
+  #####.###.#####.#.#.#.#.###.###.#.###.###  
+  #.......#.....#.#...#...............#...#  
+  #############.#.#.###.###################  
+               A O F   N                     
+               A A D   M                     ";
+
     #[test]
     fn test_star_one() {
         assert_eq!(star_one(Cursor::new(INPUT1)), 23);
@@ -264,5 +343,8 @@ YN......#               VT..#....QG
     }
 
     #[test]
-    fn test_star_two() {}
+    fn test_star_two() {
+        // assert_eq!(star_two(Cursor::new(INPUT1)), 23);
+        assert_eq!(star_two(Cursor::new(INPUT3)), 396);
+    }
 }
